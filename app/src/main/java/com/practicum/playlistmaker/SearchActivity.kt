d2b0c7be34bf388a2c2e5d0ebb4c1c79cd7ea910
Jalecti <john.n1998@yandex.ru>
2026-04-13 03:01:15 +0300

    Доработана функциональность по техническому заданию спринта 9

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,12 +1,22 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
+    private var searchText: String = ""
+    private lateinit var inputEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,61 @@ class SearchActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val searchBackButton = findViewById<ImageButton>(R.id.button_search_back)
+
+        searchBackButton.setOnClickListener {
+            finish()
+        }
+
+
+        val clearButton = findViewById<ImageView>(R.id.searchClearIcon)
+        inputEditText = findViewById<EditText>(R.id.searchInputEditText)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            inputEditText.clearFocus()
+            hideKeyboard(inputEditText)
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchText = s?.toString() ?: ""
+                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
+
+    private fun hideKeyboard(view: View) {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val restoredText = savedInstanceState.getString(SEARCH_TEXT, "")
+        inputEditText.setText(restoredText)
+    }
+
+    companion object {
+        private const val SEARCH_TEXT = "SEARCH_TEXT"
+    }
+
 }
+
+
+
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,10 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -25,5 +28,55 @@ class SettingsActivity : AppCompatActivity() {
         }
 
 
+        val settingsShareButton = findViewById<LinearLayout>(R.id.button_settings_share)
+
+        settingsShareButton.setOnClickListener {
+            openShare()
+        }
+
+        val supportButton = findViewById<LinearLayout>(R.id.button_settings_support)
+
+        supportButton.setOnClickListener {
+            openSupport()
+        }
+
+        val agreementButton = findViewById<LinearLayout>(R.id.button_settings_agreement)
+
+        agreementButton.setOnClickListener {
+            openAgreement()
+        }
+
     }
+
+    private fun openShare() {
+        val shareText = getString(R.string.share_link)
+
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, shareText)
+        }
+
+        startActivity(Intent.createChooser(intent, "Поделиться приложением"))
+    }
+
+    private fun openSupport() {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)))
+            putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_email_subject))
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.support_email_text))
+        }
+
+        startActivity(intent)
+    }
+
+    private fun openAgreement() {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(getString(R.string.practicum_offer_link))
+        }
+
+        startActivity(intent)
+    }
+
+
 }
