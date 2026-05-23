8b084160dca062ca1c4e97f88689fa620c278a55
Jalecti <john.n1998@yandex.ru>
2026-05-23 05:06:36 +0300

    Доработана функциональность по техническому заданию спринта 12

--- app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
@@ -4,9 +4,10 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class TracksAdapter (
-    private val tracks: List<Track>
-) : RecyclerView.Adapter<TracksViewHolder> () {
+class TracksAdapter(
+    private val tracks: List<Track>,
+    private val onTrackClick: (Track) -> Unit = {}
+) : RecyclerView.Adapter<TracksViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TracksViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.tracks_view, parent, false)
@@ -14,11 +15,14 @@ class TracksAdapter (
     }
 
     override fun onBindViewHolder(holder: TracksViewHolder, position: Int) {
-        holder.bind(tracks[position])
+        val track = tracks[position]
+        holder.bind(track)
+        holder.itemView.setOnClickListener {
+            onTrackClick(track)
+        }
     }
 
     override fun getItemCount(): Int {
         return tracks.size
     }
-
 }
