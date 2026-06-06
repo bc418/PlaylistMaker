ce54b6ee3e8e31ac61a04a4425b374ba33c00613
Evgeny <166113827+bc418@users.noreply.github.com>
2026-06-06 14:43:08 +0300

    Revert "Доработана функциональность по техническому заданию спринта 12"

--- app/src/main/java/com/practicum/playlistmaker/TrackDto.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackDto.kt
@@ -1,12 +1,6 @@
 package com.practicum.playlistmaker
 
-data class TrackDto(
-    val trackName: String,
-    val artistName: String,
-    val trackTimeMillis: Long,
-    val artworkUrl100: String,
-    val collectionName: String?,
-    val releaseDate: String?,
-    val primaryGenreName: String?,
-    val country: String?
-)
+data class TrackDto(val trackName: String,
+                    val artistName: String,
+                    val trackTimeMillis: Long,
+                    val artworkUrl100: String)
