699ea3f8b45fa1c184ce0e4bd5571583d33f1d8d
Jalecti <john.n1998@yandex.ru>
2026-06-06 14:40:02 +0300

    Доработана функциональность по техническому заданию спринта 13

--- app/src/main/java/com/practicum/playlistmaker/TrackMapper.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackMapper.kt
@@ -4,11 +4,14 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 
 class TrackMapper {
-    fun mapToTrack(dto: TrackDto) : Track = Track(
-            trackName = dto.trackName,
-            artistName = dto.artistName,
-            trackTime = SimpleDateFormat("mm:ss", Locale.getDefault()).format(dto.trackTimeMillis),
-            artworkUrl100 = dto.artworkUrl100
-        )
-
+    fun mapToTrack(dto: TrackDto): Track = Track(
+        trackName = dto.trackName,
+        artistName = dto.artistName,
+        trackTime = SimpleDateFormat("mm:ss", Locale.getDefault()).format(dto.trackTimeMillis),
+        artworkUrl100 = dto.artworkUrl100,
+        collectionName = dto.collectionName,
+        releaseDate = dto.releaseDate,
+        primaryGenreName = dto.primaryGenreName,
+        country = dto.country
+    )
 }
